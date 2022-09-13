5afb23ab4327cd5700ae616fe6c3ee28d3812005
joaovictor <sigma.dev.2022@gmail.com>
2022-09-13 20:23:11 -0300

    first commit

---
+++ src/test/java/com/sigma/firebolt_api/FireboltApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma.firebolt_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireboltApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
